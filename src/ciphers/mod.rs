04386ab9dda4a29ecb2204f4678c129557abd9f4
StackCrash <derek@itsecguy.com>
2018-05-12 10:11:39 -0700

    Update version of clippy used in Travis-CI

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -1,7 +1,7 @@
-pub mod caesar;
-pub mod vigenere;
-pub mod substitution;
-pub mod scytale;
-pub mod railfence;
 pub mod baconian;
+pub mod caesar;
+pub mod railfence;
 pub mod rot13;
+pub mod scytale;
+pub mod substitution;
+pub mod vigenere;
--- src/common/mod.rs
+++ src/common/mod.rs
@@ -3,7 +3,7 @@ pub const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 pub fn binary_to_char(bin: &str) -> Result<char, String> {
     let binary = bin.as_bytes().iter().map(|b| b - 48).collect::<Vec<u8>>();
     for bit in &binary {
-        if bit > &1 {
+        if *bit > 1 {
             return Err(String::from("Must be a valid binary number"));
         }
     }
